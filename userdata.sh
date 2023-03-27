d44630deef6e23779168f248a8a63fd147d6955a
paulfvc <99158260+paulfvc@users.noreply.github.com>
2023-03-27 13:55:39 +1100

    Add '-y' to add-apt-repository

--- userdata.sh
+++ userdata.sh
@@ -7,7 +7,7 @@ set -u # no unbound variables
 apt-get -qq update
 apt-get -qq install apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" -y
 
 apt-get -qq update
 apt-get -qq upgrade -y
